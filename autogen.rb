9d6c5be087948566ccb735f66aec55d7ba916283
袁新宇 <yuanxinyu@ebaolife.net>
2016-05-31 15:12:34 +0800

    自动生成所有字段的view层

--- autogen.rb
+++ autogen.rb
@@ -1,5 +1,14 @@
 ActiveRecord::Base.connection.tables.each do |t|
- clazz = t.camelize
- `rails g scaffold #{clazz} id:int`
+  model = t.singularize
+  `rails g model #{model}`
 end
+
+ActiveRecord::Base.connection.tables.each do |t|
+  clazz_name = t.camelize.singularize
+  clazz = Object.const_get(clazz_name)
+  cols = clazz.columns.delete_if{|x| x.name=="created_at" || x.name=="updated_at"}
+  fields = cols.map{|x| x.name+":"+x.type.to_s}.join(" ")
+  `rails g scaffold #{clazz} #{fields}`
+end
+
 
