78363cdb7402c97135e594a2a3989ef060b13c70
袁新宇 <yuanxinyu@ebaolife.net>
2016-06-13 15:28:24 +0800

    处理不符合rails命名规范的数据库表

--- autogen.rb
+++ autogen.rb
@@ -1,13 +1,21 @@
 ActiveRecord::Base.connection.tables.each do |t|
+  next if t.match /_\d/ #表的名字类似goodslist_20151127
   model = t.singularize
+  puts "rails g model #{model}"
   `rails g model #{model}`
+  if model == t #表的名字不是复数，不符合rails规范
+    filename = "app/models/#{model}.rb"
+    `rpl "end" "  self.table_name = '#{t}'\nend" #{filename}`
+  end
 end
 
 ActiveRecord::Base.connection.tables.each do |t|
+  next if t.match /_\d/
   clazz_name = t.camelize.singularize
   clazz = Object.const_get(clazz_name)
   cols = clazz.columns.delete_if{|x| x.name=="created_at" || x.name=="updated_at"}
   fields = cols.map{|x| x.name+":"+x.type.to_s}.join(" ")
+  puts "rails g scaffold #{clazz} #{fields}"
   `rails g scaffold #{clazz} #{fields}`
 end
 
