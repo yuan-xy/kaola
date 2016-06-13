390b22c445bed44eb733121aedcd59a03862a6a0
袁新宇 <yuanxinyu@ebaolife.net>
2016-06-13 17:03:10 +0800

    处理不符合rails命名规范的数据库表

--- autogen.rb
+++ autogen.rb
@@ -1,22 +1,36 @@
-ActiveRecord::Base.connection.tables.each do |t|
-  next if t.match /_\d/ #表的名字类似goodslist_20151127
+def gen_model(t)
   model = t.singularize
   puts "rails g model #{model}"
   `rails g model #{model}`
-  if model == t #表的名字不是复数，不符合rails规范
-    filename = "app/models/#{model}.rb"
-    `rpl "end" "  self.table_name = '#{t}'\nend" #{filename}`
-  end
 end
 
-ActiveRecord::Base.connection.tables.each do |t|
-  next if t.match /_\d/
+def gen_scaffold(t)
   clazz_name = t.camelize.singularize
   clazz = Object.const_get(clazz_name)
   cols = clazz.columns.delete_if{|x| x.name=="created_at" || x.name=="updated_at"}
   fields = cols.map{|x| x.name+":"+x.type.to_s}.join(" ")
   puts "rails g scaffold #{clazz} #{fields}"
-  `rails g scaffold #{clazz} #{fields}`
+  #`rails g scaffold #{clazz} #{fields}`  
 end
+
+def fix_table_name(t)
+  if t.singularize == t #表的名字不是复数，不符合rails规范
+    filename = "app/models/#{t}.rb"
+    `rpl "\nend" "\n  self.table_name = '#{t}'\nend" #{filename}`
+  end
+end
+
+$flag=false
+ActiveRecord::Base.connection.tables.each do |t|
+  next if t.match /_\d/ #表的名字类似goodslist_20151127
+  $flag=true if t=="so_send_store_order"
+  next unless $flag
+  gen_model(t)
+  fix_table_name(t)
+  gen_scaffold(t)
+  fix_table_name(t)
+end
+
+
 
 
