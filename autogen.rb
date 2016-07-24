1afed852abf4afa5e3c6d1bfbb0241bba50a96a6
袁新宇 <yuanxinyu@ebaolife.net>
2016-07-24 13:32:11 +0800

    外键关系

--- autogen.rb
+++ autogen.rb
@@ -1,6 +1,6 @@
 def gen_model(t)
   model = t.singularize
-  puts "rails g model #{model}"
+  Rails.logger.debug "rails g model #{model}"
   `rails g model #{model}`
 end
 
@@ -9,7 +9,7 @@ def gen_scaffold(t)
   clazz = Object.const_get(clazz_name)
   cols = clazz.columns.delete_if{|x| x.name=="created_at" || x.name=="updated_at"}
   fields = cols.map{|x| x.name+":"+x.type.to_s}.join(" ")
-  puts "rails g scaffold #{clazz} #{fields} -f"
+  Rails.logger.debug "rails g scaffold #{clazz} #{fields} -f"
   `rails g scaffold #{clazz} #{fields} -f`  
 end
 
@@ -24,12 +24,39 @@ def fix_table_name(t)
   end
 end
 
+def gen_rel(t, to)
+  single = t.singularize
+  filename = "app/models/#{single}.rb"
+  `rpl "\nend" "\n  belongs_to :#{to}\nend" #{filename}` 
+  filename = "app/models/#{to}.rb"
+  `rpl "\nend" "\n  has_many :#{t}\nend" #{filename}`   
+end
+
+def gen_relations(t)
+  clazz_name = t.camelize.singularize
+  clazz = Object.const_get(clazz_name)
+  cols = clazz.columns.find_all{|x| x.name[-3..-1]=="_id"}
+  Rails.logger.debug "try finding relationship: #{t}"
+  cols.each do |col|
+    sname = col.name[0..-4]
+    begin
+      clazz = Object.const_get(sname.camelize)
+      Rails.logger.info "found: #{t} -> #{clazz}"
+      gen_rel(t, sname)
+    rescue
+      Rails.logger.warn "not found: #{t} -> #{col.name}"
+    end
+  end
+end
+
+
 ActiveRecord::Base.connection.tables.each do |t|
   next if t.match /_\d/ #表的名字类似goodslist_20151127
   gen_model(t)
   fix_table_name(t)
   gen_scaffold(t)
   fix_table_name(t)
+  gen_relations(t)
 end
 
 
