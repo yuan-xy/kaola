dcfde0db337bcf00e077833acf092bfaa570e0c3
袁新宇 <yuanxinyu@ebaolife.net>
2016-08-09 23:59:13 +0800

    自动计算有哪几个非缺省的数据库

--- extra_databases.rb
+++ extra_databases.rb
@@ -1,1 +1,6 @@
-$extra_databases=["jbbase"]
+def get_extra_dbs
+  dbs = Rails.configuration.database_configuration.keys.find_all{|x| x.match("_development")}
+  size = "_development".size+1
+  dbs.map{|x| x[0..-size]}
+end
+$extra_databases = get_extra_dbs
