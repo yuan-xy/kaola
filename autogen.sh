f3b25f5b921fb8375005984920a0cb7c90b8b25b
袁新宇 <yuanxinyu@ebaolife.net>
2016-08-09 16:19:39 +0800

    m

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,4 @@
+#bundle exec spring binstub --all
 touch belongs.yaml
 touch many.yaml
 cp lib/templates/jbuilder/*.jbuilder ~/.rvm/gems/ruby-2.3.0/gems/jbuilder-2.4.1/lib/generators/rails/templates/
