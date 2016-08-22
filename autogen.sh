26b871d6d1dc45bae5e970db5b5e9846fdd11ec7
袁新宇 <yuanxinyu@ebaolife.net>
2016-08-23 00:22:15 +0800

    防止spring的缓存导致代码更新无效果

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 touch public/belongs.yaml
 touch public/many.yaml
 cp lib/templates/jbuilder/*.jbuilder ~/.rvm/gems/ruby-2.3.0/gems/jbuilder-2.4.1/lib/generators/rails/templates/
-spring_pid=`ps -ef | grep "spring app" | grep -v grep | awk '{print $2}'`
+spring_pid=`ps -ef | grep "spring server" | grep -v grep | awk '{print $2}'`
 if 	[ -n "$spring_pid" ]; then  
 	kill -9 $spring_pid
 fi
