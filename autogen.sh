fdd2d0f09581a6913bece8662d8a6295a700db65
袁新宇 <yuanxinyu@ebaolife.net>
2016-08-23 00:13:45 +0800

    防止spring的缓存导致代码更新无效果

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,9 @@
 touch public/belongs.yaml
 touch public/many.yaml
 cp lib/templates/jbuilder/*.jbuilder ~/.rvm/gems/ruby-2.3.0/gems/jbuilder-2.4.1/lib/generators/rails/templates/
+spring_pid=`ps -ef | grep "spring app" | grep -v grep | awk '{print $2}'`
+if 	[ -n "$spring_pid" ]; then  
+	kill -9 $spring_pid
+fi
 rails r autogen.rb 
 rails r gen_index.rb
--- incremental_update.sh
+++ incremental_update.sh
@@ -1,3 +1,7 @@
 cp lib/templates/jbuilder/*.jbuilder ~/.rvm/gems/ruby-2.3.0/gems/jbuilder-2.4.1/lib/generators/rails/templates/
+spring_pid=`ps -ef | grep "spring app" | grep -v grep | awk '{print $2}'`
+if 	[ -n "$spring_pid" ]; then  
+	kill -9 $spring_pid
+fi
 rails r autogen.rb inc_update $1
 rails r gen_index.rb
