85e46a5419f12b6758c9f9ad09bfbacb5be9d753
袁新宇 <yuanxinyu@ebaolife.net>
2016-09-20 14:45:55 +0800

    raw sql查询支持?参数

--- app/controllers/sql_controller.rb
+++ app/controllers/sql_controller.rb
@@ -3,8 +3,15 @@ class SqlController < ApplicationController
   before_action :check_rawsql_json
   
   def search
-    arr = ActiveRecord::Base.connection.select_all($raw_sqls[params[:id].to_i])
-    render :json => arr.to_json
+    sql = $raw_sqls[params[:id].to_i]
+    num = $raw_sql_params[params[:id].to_i]
+    arr = [sql]
+    if num>0
+      (1..num).each{|x| arr << params[x.to_s]}
+    end
+    r = ActiveRecord::Base.send(:sanitize_sql_array, arr)
+    ret = ActiveRecord::Base.connection.select_all r    
+    render :json => ret.to_json
   end
 
   def exec
--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,29 @@ require 'rack/cors'
 $many = YAML.load(File.read('./public/many.yaml'))
 $belongs = YAML.load(File.read('./public/belongs.yaml'))
 
+str = <<-FOO
+select  product_code,product_name,common_name,product_license,product_spec,product_unit,dosage_form,manu_facturer,kc,spell_code
+ from 
+(select id,tbw_store_id  from tpo_rm_headers where tbw_store_id=? ) b
+left join 
+(select id,tpo_rm_header_id,tbp_product_id,(rm_qty-sales_qty-rtv_qty-rtv1_qty) kc,
+batch_no,expired_date,production_date from tpo_rm_details  where (rm_qty-sales_qty-rtv_qty-rtv1_qty)>0) a
+on a.tpo_rm_header_id=b.id
+left join 
+(select id,product_code,product_name,common_name,spell_code,product_license,product_spec,product_unit,dosage_form,manu_facturer from tbp_products
+where (spell_code like ? or product_code like ? )) c
+on a.tbp_product_id=c.id
+ where  a.tpo_rm_header_id=b.id and  a.tbp_product_id=c.id
+order by expired_date
+FOO
+
 $raw_sqls=[nil,
         "select * from  tbp_products where id in (select tbp_product_id  from tbp_product_mappings)",
-        "select * from  tbp_products where id in (select tbp_product_id  from tbp_product_mappings)"
+        "select * from  tbp_products where id in (select tbp_product_id  from tbp_product_mappings)",
+        str     
       ]
 
+$raw_sql_params=[0,0,0,3]
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
