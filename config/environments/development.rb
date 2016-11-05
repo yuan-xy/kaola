71fb3896f97fa70c7816a123701f8f332f49629d
袁新宇 <yuanxinyu@ebaolife.net>
2016-11-05 18:49:19 +0800

    开发环境依赖memcache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.cache_store = :dalli_store, nil,
+    { :namespace => "DEV_API", :expires_in => 1.day, :compress => true }
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
