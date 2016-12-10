3e82ef5f111a9258ee02bd6a6fd17b77cdc7149f
袁新宇 <yuanxinyu@ebaolife.net>
2016-12-11 00:11:15 +0800

    一个对象的多个belongs对象使用multi方法一次性读取memcache缓存

--- config/initializers/cache_callback.rb
+++ config/initializers/cache_callback.rb
@@ -8,12 +8,46 @@ class ActiveRecord::Base
     Rails.cache.delete(memcache_key(self.class, self.id))
   end
   
+  def belongs_to_multi_get
+    return {} if $belongs['tbp_combin_detail'].nil?
+    ret = {}
+	  request_caches = $belongs['tbp_combin_detail'].map do |x|
+      cache,flag = self.request_cache_of_belongs_to_only(x)
+      [x,cache,flag]
+	  end
+    request_caches.each {|x, cache,flag| ret[x]=cache if flag}
+    memcache_names = request_caches.delete_if{|x, cache,flag| flag}.map{|x, cache,flag| x}
+    return ret if memcache_names.empty?
+    mem_caches = memcache_belongs_to_multi(memcache_names)
+    mem_caches.each do |k, v|
+      RequestStore.store[request_cache_key_of_belongs(k)] = v
+    end
+    ret.merge! mem_caches
+    ret
+  end
+  
+  def memcache_belongs_to_multi(arr)
+    ret = {}
+    names = arr.map{|x| memcache_cache_key_of_belongs(x)}
+    caches = Rails.cache.read_multi(*names)
+    arr.each_with_index do |method_name,i|
+      cache = caches[names[i]]
+      clazz, id = clazz_id_of_belongs(method_name)
+      ret[arr[i]] = memcache_load0(clazz, id, cache)
+    end
+    ret
+  end
+  
   def memcache_key(clazz, id)
     "#{clazz.name} #{id}"
   end
   
   def memcache_load(clazz, id)
     cache = Rails.cache.read(memcache_key(clazz, id))
+    memcache_load0(clazz, id, cache)
+  end
+  
+  def memcache_load0(clazz, id, cache)
     if cache
       if cache == nil_value(clazz,id)
         nil
@@ -34,16 +68,37 @@ class ActiveRecord::Base
     {clazz => [id,nil]}
   end
   
-  def request_cache_of_belongs_to(method_name)
+  def request_cache_key_of_belongs(method_name)
     id = self.send(method_name+"_id")
     clazz_name = get_belongs_class_name(method_name)
-    key = clazz_name+id.to_s
+    clazz_name+id.to_s
+  end
+  
+  def memcache_cache_key_of_belongs(method_name)
+    clazz, id = clazz_id_of_belongs(method_name)
+    memcache_key(clazz, id)
+  end
+
+  def request_cache_of_belongs_to_only(method_name)
+    key = request_cache_key_of_belongs(method_name)
+    return [RequestStore.store[key], RequestStore.store.has_key?(key)]
+  end
+    
+  def cache_of_belongs_to(method_name)
+    key = request_cache_key_of_belongs(method_name)
     return RequestStore.store[key] if RequestStore.store.has_key? key
-    #ret = self.send(method_name)
+    id = self.send(method_name+"_id")
+    clazz_name = get_belongs_class_name(method_name)
     ret = memcache_load(Object.const_get(clazz_name), id)
     RequestStore.store[key] = ret
     ret
   end
+  
+  def clazz_id_of_belongs(method_name)
+    id = self.send(method_name+"_id")
+    clazz_name = get_belongs_class_name(method_name)
+    [Object.const_get(clazz_name), id]
+  end  
   
   def get_belongs_class_name(method_name)
     hash = $belongs_class[self.class.name.underscore]
