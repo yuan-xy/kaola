ba10113f928764c172dd03c7f292faa21e6be2fe
袁新宇 <yuanxinyu@ebaolife.net>
2016-12-20 11:59:49 +0800

    删除重复的路由

--- autogen.rb
+++ autogen.rb
@@ -5,3 +5,34 @@ $verbose = ARGV.find{|x| x=="verbose"}
 gen_db_tables($database_tables)
 
 require_relative 'gen_relations'
+
+def uniq_routes
+  res = []
+  output = []
+  begin_res = 0
+  File.open('config/routes.rb').each do |line|
+    if begin_res == 0
+      if line =~ /resource/
+        begin_res = 1
+        res << line
+      else
+        output << line
+      end
+    elsif begin_res == 1
+      if line =~ /resource/
+        res << line
+      else
+        begin_res = 2
+        res.sort.uniq.each {|x| output << x}
+        output << line
+      end
+    elsif begin_res == 2
+      output << line
+    end
+  end
+  File.open('config/routes.rb', "wb") do |file| 
+    output.each {|x| file.write(x) }
+  end
+end
+
+uniq_routes
