804e940012bdf3ba25808f8fa4a15a2e026da2ef
scm1 <scm1@jianbaolife.com>
2017-01-09 16:56:19 +0800

    dockerfile fix

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,6 @@
-FROM registry.laobai.com/rails:4.2.6
+FROM registry.laobai.com/rails:5.0.1
 COPY . /usr/src/app
+#RUN cd /usr/src/app && bundle install
+#WORKDIR /usr/src/app
+#EXPOSE 3000/tcp
+#CMD ["/usr/src/app/run.sh"] 
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-class ActionDispatch::Routing::Mapper
-  def draw(routes_name)
-    instance_eval(File.read(Rails.root.join("config/#{routes_name}.rb")))
-  end
-end
-
-Rails.application.routes.draw do
-  draw :route_common
-end
