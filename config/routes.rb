b35ceb4130c4f51d998cb8c3866bd93c07319d7c
袁新宇 <yuanxinyu@ebaolife.net>
2017-01-12 17:18:37 +0800

    fix routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+class ActionDispatch::Routing::Mapper
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/#{routes_name}.rb")))
+  end
+end
+
+Rails.application.routes.draw do
+  draw :route_common
+end
