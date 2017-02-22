bebff7234400b85c4f0e785723732a5b08cd800d
袁新宇 <yuanxinyu@ebaolife.net>
2017-02-22 16:00:19 +0800

    m

--- config/initializers/use_index.rb
+++ config/initializers/use_index.rb
@@ -1,6 +1,8 @@
 class ActiveRecord::Base
 
   def self.use_index(index)
+    return unless index
+    raise "index str:#{index} is too long" if index.size>40
     from("#{self.table_name} USE INDEX(#{index})")
   end
    
