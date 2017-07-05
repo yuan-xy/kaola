aa798eb6ddf7679850f861a3c42153f42e0f2937
袁新宇 <yuanxinyu@ebaolife.net>
2017-07-05 16:19:47 +0800

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store, nil,
+    { :namespace => "KaolaAPI", :expires_in => 1.day, :compress => true }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
